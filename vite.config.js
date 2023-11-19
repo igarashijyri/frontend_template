b8dd72a42e04eb28db1acaa4b72f93c3377cdd9d
igarashijyri <fourleafclover688@gmail.com>
2023-11-19 21:17:25 +0900

    typeScriptだったので作り直した

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,14 @@
+import { fileURLToPath, URL } from 'node:url'
+
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 export default defineConfig({
-  plugins: [vue()],
-  server: {
-    port: 3000,
-    host: true }
+  plugins: [ vue() ],
+  server: { port: 3000, host: true },
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    }
+  }
 })
