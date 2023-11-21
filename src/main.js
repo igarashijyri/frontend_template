f360717bae9743e561bf3903a0a04f78e3f29f52
igarashijyri <fourleafclover688@gmail.com>
2023-11-21 23:20:52 +0900

    tmp

--- src/main.js
+++ src/main.js
@@ -1,6 +1,11 @@
+// エントリーポイントファイル(プログラムが実行を開始するための特定の場所やファイル)
+// 通常プログラムが始動するときに最初に実行されるコードやファイル
 import './assets/main.css'
 
 import { createApp } from 'vue'
+// 多分こいつを見ているんだけど、何故かdocker再起動しないと変更が適用されない。
+// .vueファイルに色とか何もつかなくて見づらくて仕方ないのでvscodeで良い設定ないか探してみる。
+// 1つのファイル(.vue)にhtmlとcssとjsが記述されてて地獄でしかないんだけど、切り分ける手段はないものか。
 import App from './App.vue'
 import router from './router'
 
